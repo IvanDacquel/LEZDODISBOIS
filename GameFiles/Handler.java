4d2f023a9a53fb700485c4a945137e88d562e561
Xhylan <mbdollentes@up.edu.ph>
2019-12-05 13:00:06 +0800

    Added support for signal handling from users

--- GameFiles/Handler.java
+++ GameFiles/Handler.java
@@ -12,9 +12,14 @@ public class Handler implements Runnable{
   @Override
   public void run(){
     try{
-			while(true){
-				System.out.println(this.server.receive());
-			}
-		}catch(IOException e){}
+		while(true){
+			String received = this.server.receive();
+			System.out.println(received);
+			this.server.updateLog(this.user, received);
+			this.server.handleReceived(received);
+			if (received.substring(0,2).equals("EX")) this.server.send("RL", user.getAddress(), user.getPort());
+			this.server.updateTracker();
+		}
+	}catch(IOException e){}
   }
 }

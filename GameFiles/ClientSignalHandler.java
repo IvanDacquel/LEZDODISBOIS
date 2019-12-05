f14c3deecf2d6ae4a002f7edd17a1bf1a3a822da
Xhylan <mbdollentes@up.edu.ph>
2019-12-05 13:02:14 +0800

    Enclosed receive() in an iterative loop

--- GameFiles/ClientSignalHandler.java
+++ GameFiles/ClientSignalHandler.java
@@ -6,14 +6,18 @@ public class ClientSignalHandler implements Runnable {
 	private DatagramSocket socket;
 	private String message;
 
-	private HandlerListener listener;
+	private ClientController listener;
 
-	public ClientSignalHandler(DatagramSocket socket, HandlerListener listener) {
+	public ClientSignalHandler(DatagramSocket socket, ClientController listener) {
 		this.socket = socket;
 		//	listener == ClientController
 		this.listener = listener;
 	}
 
+	/*
+	 * Change this so that the client controller would have only one Thread that reads
+	 * server messages continuously.
+	 */
 	@Override
 	public void run() {
 		message = new String();
@@ -22,32 +26,36 @@ public class ClientSignalHandler implements Runnable {
 		DatagramPacket packet = new DatagramPacket(buf, buf.length);
 
 	    try {
-	    	//	Waits for the server to send a reply
-			socket.receive(packet);
-			//	Stores the received message in "message"
-			message = new String(packet.getData(), 0, packet.getLength());
-			//	Identifies the server's commands from received message's format
-			switch(message.substring(0, 2)) {
-				//	UN for giving the User Id
-				case "UN":
-					//	Tells the controller its player's id
-					listener.setUserId(Integer.parseInt(message.substring(2, 4)));
-					break;
+			while(true){
+				//	Waits for the server to send a reply
+				socket.receive(packet);
+				//	Stores the received message in "message"
+				message = new String(packet.getData(), 0, packet.getLength());
+				//	Identifies the server's commands from received message's format
+				System.out.println(message);
+				switch(message.substring(0, 2)) {
+					//	UN for giving the User Id
+					case "UN":
+						//	Tells the controller its player's id
+						listener.setUserId(Integer.parseInt(message.substring(2, 4)));
+						break;
 				//	SC for changing the 4 cards at hand for this player
-				case "SC":
-					//	Tells the controller to change the cards to these newly received ones
-					listener.setCards(message.substring(2, 4), message.substring(4, 6), message.substring(6, 8), message.substring(8, 10));
-					break;
+					case "SC":
+						//	Tells the controller to change the cards to these newly received ones
+						listener.setCards(message.substring(2, 4), message.substring(4, 6), message.substring(6, 8), message.substring(8, 10));
+						break;
 				//	WN for signaling that the winning condition has been met
-				case "WN":
+					case "WN":
 					//	Tells the controller to activate the ENTER button
-					listener.pressEnter();
-					break;
-				default:
-					System.out.println("No match!");
+						listener.pressEnter();
+						break;
+					case "RL":
+						listener.returnToLogIn();
+						break;
+					default:
+						break;
+				}
 			}
-	        System.out.println("Received: " + message);
-
 		} catch (IOException e) {
 			e.printStackTrace();
 		}

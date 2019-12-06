180feef3655e29c9c3bb315e395586850bc312db
Xhylan <mbdollentes@up.edu.ph>
2019-12-06 18:01:32 +0800

    Revamped run() method, allotment of Handler threads to players removed

--- GameFiles/Server.java
+++ GameFiles/Server.java
@@ -1,28 +1,35 @@
+/***
+  * Server.java
+  * Authors: Ivan Amadeus Dacquel, Michael Anthony Dollentes, Francis James Figueroa, Jeran Salcedo, Paul Villaro
+  * Description: A Server object which hosts a 1-2-3 PASS! Game Server.
+ ***/
+
 import java.net.DatagramSocket;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Collections;
+import java.util.Collections; //for shuffling contents in a collection like ArrayList;
 import java.util.Map;
 import java.util.Scanner;
 
 public class Server extends UDPComponent implements Runnable, GameConstants{
-	private int gameState;
-	private int maxPlayers;
-	private ArrayList<User> players;
-	private Thread thread;
-	private HashMap<User, String> hands;
-	private HashMap<User, String> log;
-	private HashMap<User, String> passedCards;
-	private int tracker;
-	private int endgame = 0;
-	private int turn;
+	private int gameState; //stores the state of the game
+	private int maxPlayers; //maximum number of connected players
+	private ArrayList<User> players; //storage of player information
+	private HashMap<User, String> hands; //HashMap of cards in players' hands
+	private HashMap<User, String> log; //HashMap of received messages from each player for current turn
+	private HashMap<User, String> passedCards; //HashMap of passed cards by each player
+	private int tracker; //tracks the number of players who sent a message to the server.
+	private int endgame = 0; //tracks how many players pressed "Enter"
+	private int turn; //tracks the number of turns
 
 	/*
 	 * Constructs a server by binding a DatagramSocket to a specified
-	 * port. Run the server on a separate thread.
+	 * port. Run the server on a separate thread. Creates new ArrayLists
+	 * and HashMaps for the data structures. Initialize number of turns
+	 * to 0, as it is in the state of waiting for players.
 	 */
 	public Server(int port, int maxPlayers) throws IOException{
 		super(port);
@@ -32,7 +39,7 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		this.hands = new HashMap<User, String>();
 		this.log = new HashMap<User, String>();
 		this.passedCards = new HashMap<User, String>();
-		this.turn = 1;
+		this.turn = 0;
 		new Thread(this).start();
 	}
 
@@ -60,13 +67,17 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		ArrayList<String> gameDeck = new ArrayList<String>(), mainSetup;
 
 		while(deckCount < (this.players.size() * 4)){
-			gameDeck.add(this.deck[deckCount]);
+			gameDeck.add(this.deck[deckCount]); //add cards to a new ArrayList depending on the number of players
 			deckCount++;
 		}
 
-		Collections.shuffle(gameDeck);
+		Collections.shuffle(gameDeck); //shuffle the contents of the gameDeck
 		mainSetup = new ArrayList<String>();
 
+		/*
+		  For every player, get the shuffled cards and send them over to the respective
+		  players as a signal.
+		 */
 		for(int i = 0; i < this.players.size(); i++){
 			String firstFour = "";
 			for(int cardCount = 0; cardCount < 4; cardCount++) firstFour = firstFour + gameDeck.get(cardCount + (4 * i));
@@ -77,6 +88,9 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		}
 	}
 
+	/*
+	 * Initializes the passedCards HashMap.
+	 */
 	public void initPassedCards(){
 		for (Map.Entry<User, String> entry : this.hands.entrySet()){
 			this.passedCards.put(entry.getKey(), new String());
@@ -87,9 +101,10 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 	 * Prints information of all the players and their current cards.
 	 */
 	public void printPlayerStatuses(){
-		System.out.println("Turn: " + this.turn);
+		if (this.turn > 0) System.out.println("\nPlayer Hands on Turn: " + this.turn);
+		else System.out.println("\nPlayer Hands on Game Start: ");
 		for(Map.Entry<User, String> p : this.hands.entrySet()){
-			System.out.println("Player ID: " + p.getKey().getUserID() + " Name: " + p.getKey().getUsername() + " Hand: " + p.getValue());
+			System.out.println("\tPlayer ID: " + p.getKey().getUserID() + " Name: " + p.getKey().getUsername() + " Hand: " + p.getValue());
 		}
 	}
 
@@ -98,15 +113,22 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 	 * current turn.
 	 */
 	public void printPlayerLogs(){
+		System.out.println("\nSignals Received on Turn " + this.turn);
 		for(Map.Entry<User, String> p : this.log.entrySet()){
-			System.out.println("Player ID: " + p.getKey().getUserID() + " Hand: " + p.getValue());
+			System.out.println("\tPlayer ID: " + p.getKey().getUserID() + " Name: " + p.getKey().getUsername() + " Signal: " + p.getValue());
 		}
 	}
 
+	/*
+	 * Prints the passed cards of all the players sent to the server in the
+	 * current turn.
+	 */
 	public void printPlayerPasses(){
+		System.out.println("\nPassed Cards on Turn " + this.turn);
 		for(Map.Entry<User, String> p : this.passedCards.entrySet()){
-			System.out.println("Player ID: " + p.getKey().getUserID() + " Passed: " + p.getValue());
+			System.out.println("\tPlayer ID: " + p.getKey().getUserID() + " Name: " + p.getKey().getUsername() + " Passed: " + p.getValue());
 		}
+		System.out.println();
 	}
 
 	/*
@@ -130,6 +152,10 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		this.log.replace(player, signal);
 	}
 
+	/*
+	 * Updates the passedCards HashMap with the new cards passed
+	 * in the signal.
+	 */
 	public synchronized void updatePassedCards(User player, String card){
 		this.passedCards.replace(player, card);
 	}
@@ -147,7 +173,7 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 	}
 
 	/*
-	 * Returns a boolean value as a result of isFourOfAKind.
+	 * Returns a the result of isFourOfAKind.
 	 */
 	public boolean hasWon(User player, String hand){
 		return this.isFourOfAKind(hand);
@@ -164,6 +190,10 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		return null;
 	}
 
+	/*
+	 * Searches through the current hands to find which player got a
+	 * four of a kind.
+	 */
 	public boolean foundFourOfAKind(){
 		for(Map.Entry<User, String> entry : this.hands.entrySet()){
 			if(this.hasWon(entry.getKey(), entry.getValue())) return true;
@@ -184,6 +214,9 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		}
 	}
 
+	/*
+	 * Splits the string of cards into an ArrayList of lengths 2.
+	 */
 	private ArrayList<String> tokenize(String hand){
 		ArrayList<String> tokens = new ArrayList<String>();
 
@@ -230,10 +263,12 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 
 			this.hands.replace(p, newHand);
 		}
-
-		this.printPlayerStatuses();
 	}
 
+	/*
+	 * Finds the player using a user_id.
+	 * Returns null if not found.
+	 */
 	private User findPlayer(int userID){
 		for(User u : this.players){
 			if(u.getUserID() == userID) return u;
@@ -241,17 +276,35 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		return null;
 	}
 
+	/*
+	 * Finds the player using an InetAddress and a port number.
+	 * Returns null if not found.
+	 */
+	private User findPlayerByIP(InetAddress address, int port){
+		for(User u : this.players){
+			if((u.getAddress().equals(address)) && (u.getPort() == port)){
+				return u;
+			}
+		}
+		return null;
+	}
+	
+	/*
+	 * Handles received messages from each client.
+	 * Uses the first two characters as the string as the basis
+	 * of server's decision.
+	 */
 	public synchronized void handleReceived(String message) throws IOException{
 		switch(message.substring(0,2)){
-			case "PA":
+			case "PA": //update entry in passedCards hashMap with the user and its new passed card
 				int userid = Integer.parseInt(message.substring(2,4));
 				String card = message.substring(4,6);
 				this.updatePassedCards(this.findPlayer(userid), card);
 				break;
-			case "EX":
+			case "EX": //set game state to error if player disconnects during the game
 				this.setGameState(ERROR);
 				break;
-			case "EN":
+			case "EN": //four of a kind found and it's the end game phase
 				this.endgame += 1;
 				if(this.endgame < this.maxPlayers){
 					this.send("WI", this.address, this.port);
@@ -266,7 +319,7 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 	}
 
 	/*
-	 *
+	 * The server's run method.
 	 */
 	@Override
 	public void run(){
@@ -276,6 +329,11 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		User winner;
 
 		try{
+			/*
+			 * While the maximum number of players haven't connected
+			 * yet, accept connections from clients and add them to the
+			 * players ArrayList.
+			 */
 			while(this.players.size() < this.maxPlayers){
 				nickname = this.receive();
 				playerAddress = this.getAddress();
@@ -285,67 +343,69 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 				this.players.add(player);
 			}
 
-			for(User u : this.players){
-				new Thread(new Handler(this, u)).start();
-			}
-
-			this.distributeUIDs();
-			this.distributeCards();
-			this.initPassedCards();
+			this.distributeUIDs(); //distribute User ID's to respective users
+			this.distributeCards(); //distribute cards
+			this.initPassedCards(); //initialize the passed Cards hashMap
 			this.printPlayerStatuses();
 
 			boolean foundFOAK = false;
-
+			this.turn = 1; //game start! set current turn count to 1.
+			
+			/*
+			 * Receive signals from client while a four of a kind
+			 * isn't found yet or someone hasn't disconnected yet.
+			 * Print player statuses before exchanging cards.
+			 * Increment turn counter and find if a four of a kind
+			 * has already been found before beginning the loop again.
+			 */
 			while(!foundFOAK && this.gameState != ERROR){
 				this.tracker = 0;
 
-//NEED TO FIX THIS!!
-				while(this.tracker < this.maxPlayers){
-
-					System.out.println(this.tracker);
-
+				while (this.tracker < this.maxPlayers){
+					String received = this.receive();
+					User u = this.findPlayerByIP(this.getAddress(), this.getPort());
+					this.updateLog(u, received);
+					this.handleReceived(received);
+					this.updateTracker();
 				}
 
 				if(this.gameState == ERROR) break;
 
-				this.printPlayerPasses();
+				this.printPlayerStatuses();
 				this.exchangeCards();
 				this.sendCards();
-				this.printPlayerStatuses();
+				
 				foundFOAK = this.foundFourOfAKind();
 				this.turn++;
 			}
 
+			/*
+			 * Forcefully logs every one out if a user disconnected.
+			 */
 			if(this.gameState == ERROR){
 				this.broadcast("RL");
-				this.players.clear();
-				this.hands.clear();
-				this.log.clear();
-				this.passedCards.clear();
-				this.close();
-			}
+				this.refresh();
+			}else{
+			//Else, endgame is reached.
+				this.broadcast("WN");
+				this.tracker = 0;
 
-			this.broadcast("WN");
-			//this.tracker = 0;
-
-			ArrayList<User> winners = new ArrayList<User>();
-			for(Map.Entry<User, String> entry : this.hands.entrySet()){
-				if(this.isFourOfAKind(entry.getValue())){
-					winners.add(entry.getKey());
+				while (this.tracker < this.maxPlayers){
+					String received = this.receive();
+					User u = this.findPlayerByIP(this.getAddress(), this.getPort());
+					this.updateLog(u, received);
+					this.handleReceived(received);
+					this.updateTracker();
 				}
 			}
-
-			String winMes = "Winners: Player/s ";
-			for(User u : winners){
-				winMes = winMes + u.getUserID() + " ";
-			}
-			winMes = winMes + "!";
-			this.broadcast(winMes);
+			
+			this.close();
 		}catch(IOException e){
 			System.out.println("Cannot listen to " + this.socket.getPort());
 		}
 	}
 
+	//Updates the tracker.
 	public synchronized void updateTracker(){
 		this.tracker++;
 	}
@@ -368,14 +428,44 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		}
 	}
 
+	/*
+	 * Sets the current game state.
+	 */
 	private synchronized void setGameState(int state){
 		this.gameState = state;
 	}
 
+	/*
+	 * Gets the current game state.
+	 */
 	public int getGameState(){
 		return this.gameState;
 	}
 
+	/*
+	 * Clears data structures.
+	 */
+	public void refresh(){
+		this.players.clear();
+		this.hands.clear();
+		this.log.clear();
+		this.passedCards.clear();		
+	}
+	
+	/*
+	 * Clears data structures and closes the server.
+	 */
+	public void close() throws IOException{
+		this.players.clear();
+		this.hands.clear();
+		this.log.clear();
+		this.passedCards.clear();
+		super.close();		
+	}
+	
+	/*
+	 * Main program.
+	 */
 	public static void main(String[] args){
 		try{
 			Server serv = new Server(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
@@ -384,5 +474,15 @@ public class Server extends UDPComponent implements Runnable, GameConstants{
 		}catch(ArrayIndexOutOfBoundsException e){
 			System.out.println("Usage: java Server <port> <max_players>");
 		}
+	}
+	
+	/*
+	 * Have server administrator choose if he wants to reopen the server.
+	 */
+	public static int reopenServer(){
+		System.out.println("Reopen server?");
+		System.out.println("[1] Yes\t[2]No");
+		System.out.print("Choice: ");
+		return new Scanner(System.in).nextInt();
 	}
 }
